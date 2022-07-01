da34a797e568f7f0d7d394465db7b1a3b66af909
Cyrus Bakhtiari-Haftlang <cyrus@shortcut.io>
2022-07-01 14:19:47 +0200

    Dust blowing of project

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.2.1")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
---
+++ library/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    id("com.android.library")
+    `maven-publish`
+}
+
+android {
+    compileSdk = 32
+
+    defaultConfig {
+        minSdk = 22
+        targetSdk = 32
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    buildTypes.all {
+        consumerProguardFile("proguard-markdownview.pro")
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "se.ingenuity.android"
+            artifactId = "MarkdownView"
+            version = "0.0.2"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.annotation:annotation:1.4.0")
+    implementation("io.noties.markwon:core:4.6.2")
+
+    compileOnly("com.google.android.material:material:1.6.1")
+    compileOnly("androidx.appcompat:appcompat:1.4.2")
+}
